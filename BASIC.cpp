b2a13951f6b50c6bc06a968188b8a3387c0dc1ab
Andrew Shen <andrewshen31@gmail.com>
2026-02-23 10:32:25 -0800

    Div2 1081B - solved after glancing at the solution

---
+++ BASIC.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const long long NEG = (long long)-4e18;
+
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    solve();
+
+    return 0;
+}
--- div2_1081/psetB.cpp
+++ div2_1081/psetB.cpp
@@ -1,15 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <set>
-#include <unordered_set>
-#include <cmath>
-#include <algorithm>
-#include <utility>
-#include <map>
-#include <unordered_map>
-#include <functional>
-#include <array>
+#include <bits/stdc++.h>
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -18,130 +7,83 @@ const long long NEG = (long long)-4e18;
 
 using namespace std;
 
-int main()
+// useful abstractions
+// ordering of the binaries doesn't matter
+// notion of even flips is NO-OP and odd flips is FLIP
+// holding a character of a group of character offsets the same group of character flip by one
+
+// if we have even 0s and even 1s => holding all 1s => KEEP 0s and FLIP 1s
+//            even 0s and  odd 1s => impossible
+//             odd 0s and even 1s => holding all 0s => KEEP 0s and FLIP 1s
+//             odd 0s and  odd 1s => holding all 0s => KEEP 0s and FLIP 1s
+
+void solve()
 {
-    int t;
-    cin >> t;
-    vector<ll> results_ops(t);
-    vector<vector<ll>> results_indices(t);
-    for (int test_case = 0; test_case < t; ++test_case)
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    vector<int> s0;
+    vector<int> s1;
+    for (int i = 0; i < n; i++)
     {
-        vector<int> same; // 0 different, 1 same at 0, 2 same at 1
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-
-        // in reality, we do not care about the order of the binary
-        vector<ll> index0;
-        vector<ll> index1;
-        int n0 = 0;
-        int n1 = 0;
-
-        for (int i = 0; i < n; ++i)
+        if (s[i] == '0')
         {
-            if (s[i] == '0')
-            {
-                index0.push_back(i + 1);
-                n0++;
-            }
-            else
-            {
-                index1.push_back(i + 1);
-                n1++;
-            }
-        }
-
-        if (n % 2 == 0)
-        {
-            int ops;
-            bool hold1;
-            vector<ll> inds;
-            if (n0 % 2 == 0)
-            {
-                // number of 0s is even, so we flip (and we begin with holding 1)
-                ops = n - n0;
-                hold1 = true;
-            }
-            else
-            {
-                // number of 0s is odd, so no flip (and we begin with holding 0)
-                ops = n0;
-                hold1 = false;
-            }
-
-            // accordion/zipping style
-            for (int i = 0; i < ops; ++i)
-            {
-                if (hold1)
-                {
-                    inds.push_back(index1.front());
-                    index1.erase(index1.begin());
-                }
-                else
-                {
-                    inds.push_back(index0.front());
-                    index0.erase(index0.begin());
-                }
-                swap(index1, index0); // swap the indices since we are flipping the binary string after each ops
-
-                hold1 = !hold1;
-            }
-
-            results_indices[test_case] = inds;
-            results_ops[test_case] = ops;
+            s0.push_back(i + 1);
         }
         else
         {
-            if (n0 % 2 == 1)
-            {
-                vector<ll> inds;
-                bool negate = false; // starting at ops == 1 (since ops == 0 means no indices to push)
-                for (int i = 1; i <= n; ++i)
-                {
-                    int expected_n0 = negate ? n - i : i;
-
-                    if (negate)
-                    {
-                        inds.push_back(index1.front());
-                        index1.erase(index1.begin());
-                    }
-                    else
-                    {
-                        inds.push_back(index0.front());
-                        index0.erase(index0.begin());
-                    }
-                    swap(index1, index0); // swap the indices since we are flipping the binary string after each ops
-
-                    if (n0 == expected_n0)
-                    {
-                        results_ops[test_case] = i;
-                        results_indices[test_case] = inds;
-                        break;
-                    }
-
-                    negate = !negate;
-                }
-            }
-            else
-            {
-                // won't work (it goes into its own separated loop that contains all 1s that can never reach to all 0s)
-                results_ops[test_case] = -1;
-            }
+            s1.push_back(i + 1);
         }
     }
 
-    for (int i = 0; i < t; ++i)
+    if (s0.size() % 2 == 1)
     {
-        cout << results_ops[i] << endl;
-        if (results_ops[i] > 0)
+        cout << s0.size() << endl;
+        if (!s0.empty())
         {
-            for (const auto &idx : results_indices[i])
+
+            for (int i : s0)
             {
-                cout << idx << " ";
+                cout << i << " ";
             }
             cout << endl;
         }
     }
+    else if (s0.size() % 2 == 0 && s1.size() % 2 == 0)
+    {
+        cout << s1.size() << endl;
+        if (!s1.empty())
+        {
+
+            for (int i : s1)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << -1 << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll T = 1;
+
+    cin >> T;
+
+    while (T--)
+    {
+        solve();
+    }
+
     return 0;
 }
