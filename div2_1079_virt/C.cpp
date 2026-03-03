48d178332f3c52dc2275eeb10e21c48e16d1ec80
Andrew Shen <andrewshen31@gmail.com>
2026-03-03 09:41:53 -0800

    Upsolved 1079C

--- div2_1079_virt/C.cpp
+++ div2_1079_virt/C.cpp
@@ -59,78 +59,93 @@ int main()
     return 0;
 }
 
+//  7000000000000000
+// 10487275715782582
+
+// 1000000000000000000
+// 1000000000000000000
+
 void solve(ll T)
 {
+    // DBG(7000000000000000 / 2);
+    // DBG(10487275715782582 / 3);
+
     ull p, q;
     cin >> p >> q;
 
-    if (p % 2 == 0 && q % 3 == 0 && p / 2 == q / 3)
+    if (p < q && min(p / 2, q / 3) >= q - p)
     {
         cout << "Bob" << endl;
     }
-    else if (p / 2 > q / 3)
+    else
     {
-        // if the numerator is relatively larger wrt 2/3, Alice can always pick the denom.
-        // Bob cant bring the number down by picking the num. enough times
-
-        // CORRECTION: Bob CAN bring it down due to the fact that numerator can be brought down
-        // quicker (in halves) then a denominator which is in thirds.
-        // assume the optimal strat here is Alice always denom. and Bob always num.
-        // if Alice chooses num., Bob can always choose denom. to cancel the effect (preventing overshooting)
-
-        if (p % 2 == 0 && q % 3 == 1)
-        {
-            // (num offset, denom offset) (0,1) -> (0,0) -> (1,0) -> (1,2) -> (0,2) -> (0,1) -> (1,1) -> (1,0)
-            // after 5 turns same thing again but opposite turns
-
-            // this brings p to an integer factor down faster than q can
-            cout << "Alice" << endl;
-        }
-        else if (p % 2 == 0 && q % 3 == 2)
-        {
-            // same as above
-            cout << "Alice" << endl;
-        }
-        else if (p % 2 == 1 && q % 3 == 1)
-        {
-            // whatever Alice chooses, Bob can choose the remaining one with 1 offset
-            cout << "Bob" << endl;
-        }
-        else if (p % 2 == 1 && q % 3 == 2)
-        {
-            // Alice must pick p
-            cout << "Alice" << endl;
-        }
-    }
-    else if (p / 2 < q / 3)
-    {
-        // if the denom. is relatively larger wrt 2/3, Alice can always pick the num.
-        // Bob cant bring the number up by picking the denom. enough times
         cout << "Alice" << endl;
     }
-    else
-    { // p / 2 == q / 3 but each are not divisible by 2, 3 respectively (very close)
-        // so same integer k factor (up to flooring)
-        // recall, Alice gets to go first
-        if (p % 2 == 0 && q % 3 == 1)
-        {
-            // this brings p to an integer factor down faster than q can
-            cout << "Alice" << endl;
-        }
-        else if (p % 2 == 0 && q % 3 == 2)
-        {
-            // same as above
-            cout << "Alice" << endl;
-        }
-        else if (p % 2 == 1 && q % 3 == 1)
-        {
-            // whatever Alice chooses, Bob can choose the remaining one with 1 offset
-            cout << "Bob" << endl;
-        }
-        else if (p % 2 == 1 && q % 3 == 2)
-        {
-            // Alice must pick p
-            cout << "Alice" << endl;
-        }
-    }
+
+    // DBG(p / 2);
+    // DBG(q / 3);
+
+    // if (p % 2 == 0 && q % 3 == 0 && p / 2 == q / 3)
+    // {
+    //     cout << "Bob" << endl;
+    // }
+    // else if (p / 2 > q / 3)
+    // {
+    //     // if the numerator is relatively larger wrt 2/3, Alice can always pick the denom.
+    //     // Bob cant bring the number down by picking the num. enough times
+
+    //     // CORRECTION: Bob CAN bring it down due to the fact that numerator can be brought down
+    //     // quicker (in halves) then a denominator which is in thirds.
+    //     // assume the optimal strat here is Alice always denom. and Bob always num.
+    //     // if Alice chooses num., Bob can always choose denom. to cancel the effect (preventing overshooting)
+
+    //     double pair_of_turns = 6.f * ((double)p / 2.f - (double)q / 3.f);
+
+    //     // DBG("#####################################");
+    //     // DBG(pair_of_turns);
+    //     // DBG((ll)(p / 2 - pair_of_turns / 2.f));
+    //     // DBG((ll)(q / 3 - pair_of_turns / 3.f));
+
+    //     if ((ll)(p / 2 - pair_of_turns / 2.f) >= 1)
+    //     { // there's a common positive factor (for each p,q)
+    //         cout << "Bob" << endl;
+    //     }
+    //     else
+    //     {
+    //         // no common positive factor other than having one of p or q below 1
+    //         // but the game ends long before
+    //         cout << "Alice" << endl;
+    //     }
+    // }
+    // else if (p / 2 < q / 3 || p / 2 == 0 || q / 2 == 0)
+    // {
+    //     // if the denom. is relatively larger wrt 2/3, Alice can always pick the num.
+    //     // Bob cant bring the number up by picking the denom. enough times
+    //     cout << "Alice" << endl;
+    // }
+    // else
+    // { // p / 2 == q / 3 but each are not divisible by 2, 3 respectively (very close)
+    //     // so same integer k factor (up to flooring)
+    //     // recall, Alice gets to go first
+    //     if (p % 2 == 0 && q % 3 == 1)
+    //     {
+    //         // this brings p to an integer factor down faster than q can
+    //         cout << "Alice" << endl;
+    //     }
+    //     else if (p % 2 == 0 && q % 3 == 2)
+    //     {
+    //         // same as above
+    //         cout << "Alice" << endl;
+    //     }
+    //     else if (p % 2 == 1 && q % 3 == 1)
+    //     {
+    //         // whatever Alice chooses, Bob can choose the remaining one with 1 offset
+    //         cout << "Bob" << endl;
+    //     }
+    //     else if (p % 2 == 1 && q % 3 == 2)
+    //     {
+    //         // Alice must pick p
+    //         cout << "Alice" << endl;
+    //     }
+    // }
 }
